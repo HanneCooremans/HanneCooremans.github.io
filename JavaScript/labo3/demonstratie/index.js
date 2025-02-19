652ce7e3580308dce3953069043591881f3e8626
HanneCooremans <hanne.cooremans@student.vives.be>
2025-02-19 14:10:02 +0100

    fix link uitdaging

---
+++ JavaScript/labo3/demonstratie/index.js
@@ -0,0 +1,10 @@
+
+const setup = () => {
+    let btnTry = document.getElementById("btnTry");
+
+    // Event-based programming
+    btnTry.addEventListener("mouseover", mouseHover);
+    btnTry.addEventListener("click", onClick);
+    btnTry.addEventListener("mouseout", mouseOut);
+}
+window.addEventListener("load", setup);
